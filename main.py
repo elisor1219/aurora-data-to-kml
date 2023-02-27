cf393d1de2a341ee09c0ad65349058aeb6fa081d
Elias Sörstrand <elisor1219@hotmail.com>
2023-02-28 00:00:56 +0100

    Done with small script

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+import simplekml
+
+def main():
+    with open('ovation_aurora_latest.json') as f:
+        data = json.load(f)
+
+    coordinates = data['coordinates']
+    kml = simplekml.Kml()
+    kmlMax = simplekml.Kml()
+    maxIntensity = 0
+    lonMax = 0
+    latMax = 0
+    for coord in coordinates:
+        # Find the maximum intensity
+        if coord[2] > maxIntensity:
+            maxIntensity = coord[2]
+            lonMax = coord[0]
+            latMax = coord[1]
+
+        # Save the non-zero coordinates to a json file
+        # And also only the coordinates above the equator
+        if coord[2] != 0 and coord[1] > 0:
+                kml.newpoint(name=coord[2], coords=[(coord[0], coord[1])])
+
+    kmlMax.newpoint(name=maxIntensity, coords=[(lonMax, latMax)])
+    kmlMax.save('ovation_aurora_latest_max.kml')
+    kml.save('ovation_aurora_latest.kml')
+
+if __name__ == '__main__':
+    main()
+
